0ef27db3dd9ab1546cb61bc39a0ef6ab94650e6a
loc.tranvan <loc.tranvan@iMac-hieulv.local>
2022-07-08 13:18:10 +0700

    Create Module

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {type PropsWithChildren} from 'react';
+import {
+  SafeAreaView
+} from 'react-native';
+const App = () => {
+                     
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      
+    </SafeAreaView>
+  );
+};
+
+export default App;
